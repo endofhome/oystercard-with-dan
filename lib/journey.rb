5692f9d246a963bc0bb99b6be85007ef444d66d9
Tom Barnes <ruralmyth@gmail.com>
2015-10-22 15:45:41 +0100

    ch.14 initial Journey tests and methods

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,22 @@
 class Journey
 
+  attr_reader :entry_station, :exit_station
+
+  def initialize 
+    @entry_station = nil
+    @exit_station = nil
+  end
+
+  def in_journey?
+    !!entry_station
+  end
+
+  def pass_entry station
+    @entry_station = station
+  end
+
+  def pass_exit station
+    @exit_station = station
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,28 @@ require 'journey'
 
 describe Journey do
 
+  it 'in_journey defaults to false' do
+    expect(subject.in_journey?).to be false
+  end
+
+  it 'stores entry station as variable' do
+    subject.pass_entry(:entry_station)
+    expect(subject.entry_station).to eq :entry_station
+  end
+
+  it 'after passing entry station it will be in journey' do
+    subject.pass_entry(:entry_station)
+    expect(subject).to be_in_journey
+  end
+
+  it 'after passing exit station it will not be in journey' do
+    subject.pass_exit(:exit_station)
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'stores an exit station as a variable' do
+    subject.pass_exit(:exit_station)
+    expect(subject.exit_station).to eq :exit_station
+  end
+
 end
